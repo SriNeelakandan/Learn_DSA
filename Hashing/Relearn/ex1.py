7e6ee9747fc59946ab25d269f42709c4c943f371
Sri Neels <neelscr007@gmail.com>
2025-12-08 23:42:54 +0530

    relearn hashing

---
+++ Hashing/Relearn/ex1.py
@@ -0,0 +1,24 @@
+# Hashing
+# Input array: [1,3,2,1,4]
+
+# Inputs given:
+# size of ip array
+# array elements
+# no of query
+# query elements
+
+n=int(input())
+arr=list(map(int,input().split()))
+q=int(input())
+
+# Precomputing:
+# Hash Array
+HashArray = [0] * 13
+for i in range(n):
+    HashArray[arr[i]]+=1
+
+
+# Fetching
+for i in range(q):
+    x=int(input())
+    print(HashArray[x])
