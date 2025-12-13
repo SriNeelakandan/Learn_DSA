14917f8344548a935456e90b1de9b11b59b92250
Sri Neels <neelscr007@gmail.com>
2025-12-14 00:05:17 +0530

    Relearn bubblesort

--- Sorting/Relearn/bubble.py
+++ Sorting/Relearn/bubble.py
@@ -1,30 +1,29 @@
 # Bubble sort
-# select the maximum and move it to the end
-# arr=list(map(int,input().split()))
-# n=len(arr)
 
-# for i in range(n-1,1,-1):
-#     for j in range(i):
-#         if arr[j]>arr[i]:
-#             arr[i],arr[j]=arr[j],arr[i]
-#     print(arr)
-    
-#Time Complexity: O(N^2)
+# Push the maximum to the last 
+# By adjacent swapping
+
+
+def bubble_sort(x,n):
+    for i in range(n-1,-1,-1):
+        didwap= False
+        for j in range(i):
+            if x[j]>x[j+1]:
+                #swap(x[i],x[i+1])
+                didwap = True
+                x[j],x[j+1]=x[j+1],x[j]
+        if didwap == False:
+            print(f"breaked at {i}- {j}")
+            break
+        print("runs")
+
+    return x
+x=list(map(int,input().split()))
+n=len(x) # 6
+print(bubble_sort(x,n))
+
+# Time Complexity: O(N^2)
 # Space Complexity: O(1)
 
-# Recursion:
-
-def bubble_sort(i,j,n,arr):
-    if i<=1:
-        print(arr)
-        return
-    if j==i:
-        print(arr)
-        bubble_sort(i-1,0,n,arr)
-    if arr[j]>arr[i]:
-        arr[i],arr[j]=arr[j],arr[i]
-    bubble_sort(i,j+1,n,arr)
-
-arr=list(map(int,input().split()))
-n=len(arr)
-print(bubble_sort(n-1,0,n,arr))
+# # When given array is already sorted,
+# use didwap variable to make that best time complexity as O(N)
